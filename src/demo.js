773e4057bf680e9175068c722649d5fd7d7cc92b
pl12133 <krirken@gmail.com>
2016-02-24 12:56:59 -0500

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,16 @@
+'use strict';
+const partial = require('../lib/');
+
+let debug = partial(console.log.bind(console), 'DEBUGGING:');
+debug('Easy debugging messages');
+
+let delayedFiveSeconds = partial(setTimeout, undefined, 5000);
+delayedFiveSeconds(() => console.log('I take 5 seconds to print!'));
+
+let triangleArea = (base, height) => base * height / 2;
+
+let threeFootBaseTriangleArea = partial(triangleArea, 3);
+
+let fiveFootHeightTriangleArea = partial(triangleArea, undefined, 5);
+
+console.log('Triangles are equal area?: ', threeFootBaseTriangleArea(5) === fiveFootHeightTriangleArea(3));
